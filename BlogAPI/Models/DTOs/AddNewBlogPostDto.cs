27b0992d585688c7bf2c6bede4bc73493ca994cf
KissBendeguz1 <kissb@kkszki.hu>
2026-09-21 11:33:30 +0200

    httpPost

---
+++ BlogAPI/Models/DTOs/AddNewBlogPostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogAPI.Models.DTOs
+{
+    public class AddNewBlogPostDto
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public int blogId { get; set; }
+    }
+}
